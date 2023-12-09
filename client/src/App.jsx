61c950e1d5d42f76ac4a5283cd8ae93a75453466
Alireza <alirezakhaksar.freelancer@gmail.com>
2023-12-09 19:38:16 +0330

    Navbar created and setup react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { ChakraProvider } from '@chakra-ui/react'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <>
-      Our App
-    </>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          {/* <Routers></Routers> */}
+        </main>
+      </Router>
+    </ChakraProvider>
   )
 }
 
